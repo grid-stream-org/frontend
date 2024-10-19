5920ed7afd322ee367c408eaaaae8a5ed04fc7ab
Eric599 <ericcuenat@DESKTOP-TJ49HRG.wifi.unb.ca>
2024-10-19 18:17:26 -0300

    Add prettier and eslint magic

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
+import eslintPluginImport from 'eslint-plugin-import'
+import jest from 'eslint-plugin-jest'
+import prettier from 'eslint-plugin-prettier'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  js.configs.recommended,
+  {
+    files: ['**/__tests__/**', '**/*.{test,spec}.{js,jsx}'],
+    plugins: {
+      jest, // Use the jest plugin
+    },
+    ...jest.configs['flat/recommended'],
+    rules: {
+      ...jest.configs['flat/recommended'].rules,
+      'jest/prefer-expect-assertions': 'off',
+    },
+  },
+  {
+    ignores: ['dist'],
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -17,21 +39,38 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: {
+        version: '18.3',
+      },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
+      'import': eslintPluginImport,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'off',
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react/prop-types': 'off', // Disable prop-types rule
+      'prettier/prettier': ['error'],
+      ...prettierConfig.rules,
+      'import/order': [
+        'error',
+        {
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          'alphabetize': {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
       ],
     },
   },
