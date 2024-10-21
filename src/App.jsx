60bf2a060ed199dc695bcfc733f68c4ec6ade391
Cooper <cdickson@unb.ca>
2024-10-21 19:39:17 -0300

    adding login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
 import AuthProvider from './state/AuthProvider/AuthProvider'
 import Homepage from './views/Homepage'
+import LoginForm from './components/LoginForm/LoginForm'; 
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Routes>
           {/* Public Routes*/}
           {/* <Route path="/login" element={<LoginForm />} /> */}
-          <Route path="/login" element={<Homepage />} /> {/** Render homepage for now */}
+          <Route path="/login" element={<LoginForm />} /> {/** Render homepage for now */}
           {/* Protected Routes */}
           <Route element={<ProtectedRoutes />}>
             <Route path="/" element={<Homepage />} />
